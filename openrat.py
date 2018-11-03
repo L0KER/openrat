cbd196b486a3613b0c1e077e5c135007624c2b09
L0KER <44724465+L0KER@users.noreply.github.com>
2018-11-03 14:08:10 +0300

    Create openrat.py

---
+++ openrat.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import os
+import sys
+print "*********************************************"
+print "*  ____                   _____       _     *"
+print "* / __ \                 |  __ \     | |    *"
+print "*| |  | |_ __   ___ _ __ | |__) |__ _| |_   *"
+print "*| |  | | '_ \ / _ \ '_ \|  _  // _` | __|  *"
+print "*| |__| | |_) |  __/ | | | | \ \ (_| | |_   *"
+print "* \____/| .__/ \___|_| |_|_|  \_\__,_|\__|  *"
+print "*       | |                                 *"
+print "*       |_|                                 *"
+print "*                                 v.1.0     *"
+print "*             Created by L0(KER#_#          *"
+print "*********************************************"
+print "Enter your local ip"
+drpri = raw_input("openrat>")
+with open ('client.txt', 'r') as f:
+  old_data = f.read()
+
+new_data = old_data.replace('127.0.0.1', drpri)
+
+with open ('client.txt', 'w') as f:
+  f.write(new_data)
+os.rename('client.txt', 'client.py') 
+
+with open ('connect.txt', 'r') as f:
+  old_data = f.read()
+
+new_data = old_data.replace('127.0.0.1', drpri)
+
+with open ('connect.txt', 'w') as f:
+  f.write(new_data)
+  
+os.rename('connect.txt', 'connect.py')
+print ('**********************************')
+print ('Creation was successful')
+print ('For connect open script connect.py')
+print ('**********************************')
+print ('Create new?')
+print ('1) Yes')
+print ('2) No, exit')
+a = int(input('openrat>'))
+if a < 2:
+    print('Please, restart the openrat!')
+elif a < 3:
+    print('Bye!')
